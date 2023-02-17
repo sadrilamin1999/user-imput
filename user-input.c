806677bf00f2af3e7261dc76c36a4cee9c03e038
Sadril Amin <sadrilshuvo420@gmail.com>
2023-02-17 16:15:59 +0600

    user input added

---
+++ user-input.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+  int num1;
+  float num2;
+  printf("Enter intager & float number: ");
+  scanf("%d %f", &num1, &num2);
+  printf("You have prassed :%d %.1f\n",num1, num2);
+  getch();
+}
